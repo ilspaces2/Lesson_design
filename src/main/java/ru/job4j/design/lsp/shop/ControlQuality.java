976ea58a5b22f1181f740807e4f21d4959795fbd
I_M <moskva.88@mail.ru>
2022-05-04 23:17:23 +0300

    1. Динамическое перераспределение продуктов [#854]

--- src/main/java/ru/job4j/design/lsp/shop/ControlQuality.java
+++ src/main/java/ru/job4j/design/lsp/shop/ControlQuality.java
@@ -1,6 +1,10 @@
 package ru.job4j.design.lsp.shop;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ControlQuality {
 
@@ -18,5 +22,12 @@ public class ControlQuality {
         if (foods == null || stores == null || foods.isEmpty() || stores.isEmpty()) {
             throw new IllegalArgumentException("Error list");
         }
+    }
+
+    public void resort(List<Store> stores) {
+        List<Food> foods = stores.stream()
+                .flatMap(store -> store.findAll().stream())
+                .collect(Collectors.toList());
+        controlFood(foods, stores);
     }
 }
